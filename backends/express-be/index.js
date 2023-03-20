2c1a4aa60e3f52b2517cadb20a6f8dd4b2df58c1
Nima Yonten <92887110+unimafy@users.noreply.github.com>
2023-03-21 05:30:47 +0600

    add api gateway

--- backends/express-be/index.js
+++ backends/express-be/index.js
@@ -2,10 +2,9 @@ const express =  require("express");
 
 const app = express();
 
-app.get("/express", (req, res) => {
+app.get("/api/express/", (req, res) => {
   res.send("I am the Express Backend!");
 });
-
 
 app.listen(4000, ()=> {
   console.log("listening on http://localhost:4000")
